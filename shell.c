1c111802369eaab953dc194d6b2da77b05a9b306
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2025-07-26 20:49:16 +1000

    definitely lost: 503,327,145 bytes in 100,665,429 blocks

--- shell.c
+++ shell.c
@@ -4,9 +4,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <errno.h>
 char builtin_com[15][10] = {"cd", "help", "exit"};
+// input (work in progress)
+char readln(char **input, size_t size) {
+  int rows;
+  int collumns;
+  int bytes;
+  while (1) {
+    read(1, (char *)input[rows][collumns], 1);
+    bytes++;
+    if (input[rows][collumns] == ' ') {
+      input[rows][collumns++] = '\0';
+      rows++;
+      collumns = 0;
+    } else if (input[rows][collumns] == '\n') {
+      input[rows][collumns++] = '\0';
+      return bytes;
+    } else if (collumns == (int)size-1) {
+      input[rows][collumns++] = '\0';
+      rows++;
+    } else if ((collumns == (int)size-1) && (rows == (int)size)) {
+	return 2; //exit code 2 means that it was succesful in reading bytes, but exited due to input about to be overflowed
+      }
+    collumns++;
+  }
+}
 // the commands
-  int shell_cd(char *args[100], char* pwd) {
+  int shell_cd(char args[100][100], char* pwd) {
     if (args[1] == NULL) {
       printf("Expected atleast 1 argument\n");
     }
@@ -22,27 +47,31 @@ int shell_help() {
   return 0;
 }
 
-int shell_exit(char *arguments[100], char *pwd) {
-          printf("test");
-	  //free(arguments);
+int shell_exit(char arguments[100][100], char *pwd) {
+	  free(arguments);
 	  //free(pwd);
   exit(0);
 }
 
 int shell_execute(char *com, char *args[100]) {
   int status;
+  int error;
   pid_t spoon = fork();
   if (spoon == 0) {
   fflush(stdout);
-      if (execvp(com, args) == -1) {
+  error = execvp(com, args);
+      if (error == -1) {
 	printf("No such command: %s\n", com);
+	exit(errno);
     }
         fflush(stdout);
-      return -1;
 	} else if (spoon < 0) {
     printf("Error forking\n");
     return -1;
   } else {
+    if(WIFEXITED(status)) {
+         printf("child exited with = %d\n",WEXITSTATUS(status));
+    }
         do {
       waitpid(spoon, &status, WUNTRACED);
     } while (!WIFEXITED(status) && !WIFSIGNALED(status));
@@ -50,7 +79,7 @@ int shell_execute(char *com, char *args[100]) {
   return 0;
 }
 // Handling of the commands
-  int shell(char *command, char *arguments[100], char *pwd) {
+  int shell(char *command, char arguments[100][100], char *pwd) {
     int i;
   for (i = 0; i < 4; i++) {
     if (strcmp(command, builtin_com[i]) == 1) {
@@ -71,7 +100,7 @@ int shell_execute(char *com, char *args[100]) {
     if (command == NULL) {
       return 1;
     } else {
-      shell_execute(command, arguments);
+      shell_execute(command, (char **)arguments);
     }
   }
 }
@@ -81,9 +110,9 @@ int main() {
   char hostname[25];
   char *user = getenv("USER");
   char comarg[100][100]; // I'm praying this works, If it doesn't I'll hug my blahaj and cry myself to sleep tonight // I will be crying myself to sleep tonight
-  char *pwd = (char*)malloc(100 * sizeof(char*));
+  char *pwd = getenv("PWD");
   FILE *test;
-    int x;
+    int x = 0;
     // grabbing the host name
     test = fopen("/etc/hostname", "r");
     fgets(hostname, 25, test);
@@ -94,16 +123,18 @@ int main() {
       pwd = getenv("PWD");
       printf("[%s@%s %s]$ ",user,hostname,pwd);
       fflush(stdout);
-    read(1, input, 100);
+      //readln(comarg, 100);
+      read(1, input, 100);
     // tokenization/processing
     char *thing = strtok(input, " \n");
+    x = 0;
     while (thing != NULL) {
-      strncpy(comarg[x], thing, 100); // I love valgrind and gdb //kill me // why
+      strncpy(comarg[x], thing, 100); // I love valgrind and gdb //kill me // why //fixed
           x++;
     thing = strtok(NULL, "/,.-");
     }
-    x = 0;
-    //spamification (I could go for some spam ham actually)
+    comarg[x][0] = '\0';
+    //spamification/more processing (I could go for some spam ham actually)
     x = x - 1;
     char com[100];
     strncpy(com, comarg[0], 100);
@@ -115,7 +146,19 @@ int main() {
 	a++;
       }
       printf(" %s\n", arg[0]); */ //my code works where I write it, and then I don't touch it. Otherwise it'll break somehow
-      shell(com, (char**)comarg, pwd);
+      int t = 0;
+      char **comarg2 = calloc(100,sizeof(char*));
+      printf("comarg: %p comarg2: %p\n", &comarg, &comarg2);
+      while (1) {
+	//strncpy(comarg2[t], comarg[t], 100);
+	comarg2[t] = strndup(comarg[t], 100);
+	if (comarg[t] == NULL) {
+	  break;
+	}
+      }
+      printf(" %s %s", comarg2[0], comarg2[1]);
+      return 0;
+      shell(com, comarg, pwd);
     }
 }
 
